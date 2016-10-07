026e36ac4f0e6b66d54a81a5598a6ce6a7a39eda
wkwang <252733378@qq.com>
2016-10-07 19:46:59 +0800

    add demo

---
+++ pages/today/today.js
@@ -0,0 +1,24 @@
+var util = require('../../utils/util.js')
+Page({
+  data:{
+    // text:"这是一个页面"
+    time:0
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+    var date = Date.now()
+    this.setData({time:util.formatTime(new Date(date))})
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
